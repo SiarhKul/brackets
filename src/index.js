7fe675e92556b8297e087bd21e0f1e087dfe7491
SiarhKul <SiarhKul@gmail.com>
2021-01-16 15:50:04 +0300

    init:brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arr = []
+    for (const elArr of bracketsConfig) {
+      arr.push(elArr.join(''))
+    }
+    console.log(arr);
+
+    for (let i = 0; i < arr.length; i++) {
+      if (str.includes(arr[i])) {
+        str = str.replace(arr[i], '');
+        i = -1;
+      }
+    }
+
+    if (str.length === 0) {
+      return true
+    } else {
+      return false
+    }
+
 }
+
